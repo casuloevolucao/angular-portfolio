c3fb4d3ef3be9f3b654c9a56e332c7c438806a2f
isamelalvesit <cearaismael1997@gmail.com>
2018-08-17 06:44:01 -0300

    v1.0

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { TeamComponent } from './team/team.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent,
+    ProjetosComponent,
+    TeamComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
